be4d5c32f3f60dac47d945d0d7d12acb3ab9d26e
Neeraj1684 <76860453+Neeraj1684@users.noreply.github.com>
2021-01-04 18:02:47 +0530

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,27 @@
+cricket_players = []
+
+cricket_players.append("ajinkya")
+cricket_players.append("virat")
+cricket_players.append("jadeja")
+cricket_players.append("bumrah")
+cricket_players.append("bhuvneshwar")
+print(cricket_players)
+
+for cricket_player in cricket_players:
+  print(cricket_player)
+
+if cricket_player == "bhuvneshwar" in cricket_players:
+  print("bhuvneshwar found")
+  
+else :
+  print("not found")
+  
+  output
+  
+#  ['ajinkya', 'virat', 'jadeja', 'bumrah', 'bhuvneshwar']
+#ajinkya
+#virat
+#jadeja
+#bumrah
+#bhuvneshwar
+#bhuvneshwar found
